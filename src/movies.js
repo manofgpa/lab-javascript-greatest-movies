edf611e3f6f92ec75d416891ba466570ca3c372e
Felipe Mano <manofgpa@gmail.com>
2021-10-20 18:03:54 -0300

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,4 +1,5 @@
 const movies = require('./data')
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -8,7 +9,9 @@ const getAllDirectors = (arr) => [...(new Set(arr.map(movie => movie.director)))
 
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-const howManyMovies = arr => arr.filter(movie => (movie.genre.includes('Drama') && movie.director === 'Steven Spielberg')).length
+const howManyMovies = arr => arr
+  .filter(movie => (movie.genre.includes('Drama') && movie.director === 'Steven Spielberg'))
+  .length
 
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
@@ -49,29 +52,69 @@ function dramaMoviesScore(arr) {
     return 0
   }
 
-
   return Math.round((total / dramaMoviesLength) * 100) / 100
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(arr) {
-  const sortedArray = [...arr]
+const orderByYear = (arr) => [...arr]
+  .sort((a, b) => a.year > b.year ? 1 : -1)
 
 
-  return sortedArray.sort((a, b) => a.year > b.year ? 1 : -1)
-}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(arr) {
-  const sortedArray = arr.sort((a, b) => a.year > b.year ? 1 : -1)
-}
+const orderAlphabetically = (arr) => [...arr]
+  .sort((a, b) => a.title > b.title ? 1 : -1)
+  .slice(0, 20)
+  .map(movie => movie.title)
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() { }
+function turnHoursToMinutes(arr) {
+  const minutesMovies = arr.map(movie => {
+    const newDuration = movie.duration.split(' ')
+    const hours = newDuration[0].replace('h', '')
+    const minutes = newDuration[1] ? newDuration[1].replace('min', '') : 0
+    const duration = +hours * 60 + +minutes
+
+
+    return {
+      ...movie,
+      duration
+    }
+  })
+
+  return minutesMovies
+}
+
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() { }
+function bestYearAvg(arr) {
 
+  if (arr.length === 0) {
+    return null
+  }
+
+  const uniqueYears = [...new Set(arr.map(movie => movie.year))].sort()
+  let moviesByYearArray = []
+
+  uniqueYears.forEach(year => {
+    moviesByYearArray = [...moviesByYearArray, arr.filter(movie => {
+      return movie.year === year
+    })]
+  })
+
+  let results = []
+
+  moviesByYearArray.forEach(year => {
+    results = [...results, { name: year[0].year, averageScore: year.reduce((acc, cur) => acc + cur.score, 0) / year.length }]
+  })
+
+  const bestYear = results.sort((a, b) => a.averageScore > b.averageScore ? -1 : 1)[0]
+
+
+  return `The best year was ${bestYear.name} with an average score of ${bestYear.averageScore}`
+
+}
 
 
 // The following is required to make unit tests work.
